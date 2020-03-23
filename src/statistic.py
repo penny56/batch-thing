030923b166dace3868038dc2c4115a0115407757
penny56 <penny56@163.com>
2020-03-23 11:55:31 +0800

    Update mail list.

--- src/statistic.py
+++ src/statistic.py
@@ -23,8 +23,8 @@ class statistic:
         self.mailHost = 'na.relay.ibm.com'
         self.mailSubject = '[' + configComm.sectionDict['connection']['cpc'] + ' statistic] - [' + str(date.today()) + ']'
         self.mailFrom = 'DPM_Auto'
-        self.mailTo = ['mayijie@cn.ibm.com', 'liwbj@cn.ibm.com', 'lbcruz@us.ibm.com', 'jrossi@us.ibm.com', 'w8a3m8t5g8q5q3g0@ibm-systems-z.slack.com']
-        #self.mailTo = ['mayijie@cn.ibm.com']
+        #self.mailTo = ['mayijie@cn.ibm.com', 'liwbj@cn.ibm.com', 'lbcruz@us.ibm.com', 'jrossi@us.ibm.com', 'w8a3m8t5g8q5q3g0@ibm-systems-z.slack.com']
+        self.mailTo = ['w8a3m8t5g8q5q3g0@ibm-systems-z.slack.com']
         self.content = ''
 
     def changePartitionStatus(self, cf):
